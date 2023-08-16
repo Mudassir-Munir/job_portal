eb256e7ef8b32fe2c6e0160daf1a48df7b0d7655
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 <mudassirmunir25@gmail.com>
2023-08-16 14:56:50 +0500

    app security

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { loginController, registerController } from "../handlers/auth.js";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+ windowMs: 15 * 60 * 1000, // 15 minutes
+ max: 50, // limit each Ip to 50 requests per window(here per 15 minutes)
+ standardHeaders: true,
+ legacyHeaders: false,
+});
 
 // router object
 const router = express.Router();
 
 // routes
 // register
-router.post("/register", registerController);
+router.post("/register", limiter, registerController);
 // login
-router.post("/login", loginController);
+router.post("/login", limiter, loginController);
 
 export default router;
